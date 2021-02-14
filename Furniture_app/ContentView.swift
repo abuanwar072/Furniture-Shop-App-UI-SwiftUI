25fc15cbf0af92fe28d10cbddafeb71ea0076967
Abu Anwar <abuanwar072@gmail.com>
2021-02-14 08:24:10 +0600

    Initial Commit

---
+++ Furniture_app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Furniture_app
+//
+//  Created by Abu Anwar MD Abdullah on 14/2/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Furniture_app/Furniture_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Furniture_appApp.swift
+//  Furniture_app
+//
+//  Created by Abu Anwar MD Abdullah on 14/2/21.
+//
+
+import SwiftUI
+
+@main
+struct Furniture_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
