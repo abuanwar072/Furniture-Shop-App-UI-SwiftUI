48ddfc37c68ec7d6cc214b9bc9a19e2e9e2cd855
Abu Anwar <abuanwar072@gmail.com>
2021-02-15 13:58:51 +0600

    home screen done

---
+++ Furniture_app/Screens/HomeScreen.swift
@@ -0,0 +1,223 @@
+//
+//  HomeScreen.swift
+//  Furniture_app
+//
+//  Created by Abu Anwar MD Abdullah on 14/2/21.
+//
+
+import SwiftUI
+
+struct HomeScreen: View {
+    @State private var search: String = ""
+    @State private var selectedIndex: Int = 1
+    
+    private let categories = ["All", "Chair", "Sofa", "Lamp", "Kitchen", "Table"]
+    var body: some View {
+        ZStack {
+            Color(#colorLiteral(red: 0.937254902, green: 0.937254902, blue: 0.937254902, alpha: 1))
+                .ignoresSafeArea()
+            
+            
+            
+            ScrollView (showsIndicators: false) {
+                VStack (alignment: .leading) {
+                    
+                    AppBarView()
+                    
+                    TagLineView()
+                        .padding()
+                    
+                    SearchAndScanView(search: $search)
+                    
+                    ScrollView (.horizontal, showsIndicators: false) {
+                        HStack {
+                            ForEach(0 ..< categories.count) { i in
+                                Button(action: {selectedIndex = i}) {
+                                    CategoryView(isActive: selectedIndex == i, text: categories[i])
+                                }
+                            }
+                        }
+                        .padding()
+                    }
+                    
+                    Text("Popular")
+                        .font(.custom("PlayfairDisplay-Bold", size: 24))
+                        .padding(.horizontal)
+                    
+                    ScrollView (.horizontal, showsIndicators: false) {
+                        HStack (spacing: 0) {
+                            ForEach(0 ..< 4) { i in
+                                ProductCardView(image: Image("chair_\(i+1)"), size: 210)
+                            }
+                            .padding(.leading)
+                        }
+                    }
+                    .padding(.bottom)
+                    
+                    Text("Best")
+                        .font(.custom("PlayfairDisplay-Bold", size: 24))
+                        .padding(.horizontal)
+                    
+                    ScrollView (.horizontal, showsIndicators: false) {
+                        HStack (spacing: 0) {
+                            ForEach(0 ..< 4) { i in
+                                ProductCardView(image: Image("chair_\(4-i)"), size: 180)
+                            }
+                            .padding(.leading)
+                        }
+                    }
+                    
+                }
+            }
+            
+            VStack {
+                Spacer()
+                BottomNavBarView()
+            }
+        }
+    }
+}
+
+struct HomeScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeScreen()
+    }
+}
+
+
+
+struct AppBarView: View {
+    var body: some View {
+        HStack {
+            Button(action: {}) {
+                Image("menu")
+                    .padding()
+                    .background(Color.white)
+                    .cornerRadius(10.0)
+            }
+            
+            Spacer()
+            
+            Button(action: {}) {
+                Image(uiImage: #imageLiteral(resourceName: "Profile"))
+                    .resizable()
+                    .frame(width: 42, height: 42)
+                    .cornerRadius(10.0)
+            }
+        }
+        .padding(.horizontal)
+    }
+}
+
+struct TagLineView: View {
+    var body: some View {
+        Text("Find the \nBest ")
+            .font(.custom("PlayfairDisplay-Regular", size: 28))
+            .foregroundColor(Color("Primary"))
+            + Text("Furniture!")
+            .font(.custom("PlayfairDisplay-Bold", size: 28))
+            .fontWeight(.bold)
+            .foregroundColor(Color("Primary"))
+    }
+}
+
+struct SearchAndScanView: View {
+    @Binding var search: String
+    var body: some View {
+        HStack {
+            HStack {
+                Image("Search")
+                    .padding(.trailing, 8)
+                TextField("Search Furniture", text: $search)
+            }
+            .padding(.all, 20)
+            .background(Color.white)
+            .cornerRadius(10.0)
+            .padding(.trailing, 8)
+            
+            Button(action: {}) {
+                Image("Scan")
+                    .padding()
+                    .background(Color("Primary"))
+                    .cornerRadius(10.0)
+            }
+        }
+        .padding(.horizontal)
+    }
+}
+
+struct CategoryView: View {
+    let isActive: Bool
+    let text: String
+    var body: some View {
+        VStack (alignment: .leading, spacing: 0) {
+            Text(text)
+                .font(.system(size: 18))
+                .fontWeight(.medium)
+                .foregroundColor(isActive ? Color("Primary") : Color.black.opacity(0.5))
+            if (isActive) { Color("Primary")
+                .frame(width: 15, height: 2)
+                .clipShape(Capsule())
+            }
+        }
+        .padding(.trailing)
+    }
+}
+
+struct ProductCardView: View {
+    let image: Image
+    let size: CGFloat
+    
+    var body: some View {
+        VStack {
+            image
+                .resizable()
+                .frame(width: size, height: 200 * (size/210))
+                .cornerRadius(20.0)
+            Text("Luxury Swedian chair").font(.title3).fontWeight(.bold)
+            
+            HStack (spacing: 2) {
+                ForEach(0 ..< 5) { item in
+                    Image("star")
+                }
+                Spacer()
+                Text("$1299")
+                    .font(.title3)
+                    .fontWeight(.bold)
+            }
+        }
+        .frame(width: size)
+        .padding()
+        .background(Color.white)
+        .cornerRadius(20.0)
+        
+    }
+}
+
+
+struct BottomNavBarView: View {
+    var body: some View {
+        HStack {
+            BottomNavBarItem(image: Image("Home"), action: {})
+            BottomNavBarItem(image: Image("fav"), action: {})
+            BottomNavBarItem(image: Image("shop"), action: {})
+            BottomNavBarItem(image: Image("User"), action: {})
+        }
+        .padding()
+        .background(Color.white)
+        .clipShape(Capsule())
+        .padding(.horizontal)
+        .shadow(color: Color.blue.opacity(0.15), radius: 8, x: 2, y: 6)
+    }
+}
+
+struct BottomNavBarItem: View {
+    let image: Image
+    let action: () -> Void
+    var body: some View {
+        Button(action: action) {
+            image
+                .frame(maxWidth: .infinity)
+        }
+    }
+}
